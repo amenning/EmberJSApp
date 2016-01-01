e317e4fc73a061297d9f9f0454feb6c832c25658
Andrew Menning <menning23@gmail.com>
2016-01-01 16:50:23 -0500

    After Section 2.4 Controller Basics

--- app.js
+++ app.js
@@ -1,7 +1,16 @@
 var App = Ember.Application.create({
   LOG_TRANSITIONS: true
 });
+
 App.Router.map(function() {
   this.route('about');
   this.route('credits', {path: 'thanks'});
+});
+
+App.IndexController = Ember.Controller.extend({
+	productsCount: 6,
+	logo: 'images/logo.png',
+	time: function(){
+		return (new Date()).toDateString()
+	}.property()
 });
