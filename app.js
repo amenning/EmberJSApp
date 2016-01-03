b915b7ba8c25de56d7e312e8fea608f814090ba7
Andrew Menning <menning23@gmail.com>
2016-01-03 01:02:09 -0500

    After Section 3.6 Dynamic Routes

--- app.js
+++ app.js
@@ -1,72 +1,77 @@
 var App = Ember.Application.create({
   LOG_TRANSITIONS: true
 });
-
 App.Router.map(function() {
+  this.route('credits', { path: '/thanks' });
   this.route('about');
-  this.route('credits', {path: 'thanks'});
   this.resource('products');
+  this.resource('product', { path: '/products/:title' });
   this.resource('contacts');
+  this.resource('contact', {path: '/contacts/:name'});
 });
 
 App.IndexController = Ember.Controller.extend({
-	productCount: 6,
-	logo: 'images/logo-small.png',
-	time: function(){
-		return (new Date()).toDateString()
-	}.property()
+  productsCount: 6,
+  logo: 'images/logo-small.png',
+  time: function() {
+    return (new Date()).toDateString();
+  }.property()
 });
-
 App.AboutController = Ember.Controller.extend({
-  contactName: '',
+  contactName: 'Anostagia',
   avatar: 'images/avatar.png',
-  open: function(){
-    if((new Date()).getDay() !== 0){
-    	return 'Open';
-    }else{
-    	return 'Closed';
-    }
+  open: function() {
+    return ((new Date()).getDay() === 0) ? "Closed" : "Open";
   }.property()
 });
 
-App.PRODUCTS = [
-	{
-		title: 'Flint',
-		price: 99,
-		description: 'Flint is...',
-		isOnSale: true,
-		image: 'images/products/flint.png'
-	},
-	{
-		title: 'Kindling',
-		price: 249,
-		description: 'Easily...',
-		isOnSale: false,
-		image: 'images/products/kindling.png'
-	}	
-];
-
 App.ProductsRoute = Ember.Route.extend({
-	model: function(){
-		return App.PRODUCTS;
-	}
+  model: function() {
+    return App.PRODUCTS;
+  }
 });
+App.ProductRoute = Ember.Route.extend({
+  model: function(params) {
+    return App.PRODUCTS.findBy('title', params.title); 
+  }
+});
+App.ContactsRoute = Ember.Route.extend({
+  model: function() {
+    return App.CONTACTS;
+  }
+});
+App.ContactRoute = Ember.Route.extend({
+  model: function(params){
+  	return App.CONTACTS.findBy('name', params.name);
+  }
+});
+
+App.PRODUCTS = [
+  {
+    title: 'Flint',
+    price: 99,
+    description: 'Flint is a hard, sedimentary cryptocrystalline form of the mineral quartz, categorized as a variety of chert.',
+    isOnSale: true,
+    image: 'images/products/flint.png'
+  },
+  {
+    title: 'Kindling',
+    price: 249,
+    description: 'Easily combustible small sticks or twigs used for starting a fire.',
+    isOnSale: false,
+    image: 'images/products/kindling.png'
+  }
+];
 
 App.CONTACTS = [
   {
-    name: '',
-    avatar: 'images/contacts/giamia.png',
-    about: ''
+    name: 'Giamia',
+    about: 'Although Giamia came from a humble spark of lightning, he quickly grew to be a great craftsman, providing all the warming instruments needed by those close to him.',
+    avatar: 'images/contacts/giamia.png'
   },
   {
-    name: '',
-    avatar: 'images/contacts/anostagia.png',
-    about: ''
+    name: 'Anostagia',
+    about: 'Knowing there was a need for it, Anostagia drew on her experience and spearheaded the Flint & Flame storefront. In addition to coding the site, she also creates a few products available in the store.',
+    avatar: 'images/contacts/anostagia.png'
   }
 ];
-
-App.ContactsRoute = Ember.Route.extend({
-  model: function(){
-  	return App.CONTACTS;
-  }
-});
